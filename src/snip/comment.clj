d993053fb317e1029bc3ac7829c8f6d894f637e9
Ashok Menon <amenon94@gmail.com>
2014-08-17 23:10:19 +0100

    Comments: Adding some default language -> comment mappings.

---
+++ src/snip/comment.clj
@@ -0,0 +1,16 @@
+(ns sniper.comment)
+
+(def
+  ^{:doc "Map from languages to comment delimiters."}
+  lang->cmnt
+  {"clj"  ";;"
+   "hs"   "--"
+   "rb"   "#"
+   "py"   "#"
+   "js"   "//"
+   "c"    "//"
+   "h"    "//"
+   "hpp"  "//"
+   "cpp"  "//"
+   "m"    "//"
+   "java" "//"})
