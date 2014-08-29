81817bd113cde0740c10fb2bf2df4911d0bb5a18
Ashok Menon <amenon94@gmail.com>
2014-08-29 23:59:19 +0100

    Args: Adding recursive property.
     * Also changing the argument processing logic: Always print errors,
       print summary if the help flag is present or there are errors
       present.

--- src/snip/core.clj
+++ src/snip/core.clj
@@ -10,19 +10,17 @@
   [["-o" "--output" "Output directory, defaults to the current directory"
     :default "."
     :parse-fn normalize-path]
+   ["-r" "--recursive" "Treat each file/directory recursively"]
    ["-h" "--help" "Print this help summary"]])
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (let [{dirs :arguments
-         opts :options
-         help :summary
-         err :errors} (parse-opts args cli-opts)]
+  (let [{files :arguments
+         opts  :options
+         help  :summary
+         err   :errors} (parse-opts args cli-opts)]
+    (doseq [e err] (println e))
     (cond
-      (seq err)
-      (do (doseq [e err] (println e))
-          (println help))
-
-      (:help opts)
+      (or (:help opts) (seq err))
       (println help))))
