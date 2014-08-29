f05f283ae82a7d96c76f9487c2297322bcd4535b
Ashok Menon <amenon94@gmail.com>
2014-08-29 23:55:19 +0100

    Args: Added argument processing.

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :url "https://github.com/asQuirreL/snip"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.cli "0.3.1"]]
   :main ^:skip-aot snip.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/snip/core.clj
+++ src/snip/core.clj
@@ -1,7 +1,28 @@
 (ns snip.core
+  (:require [clojure.java.io :as io])
+  (:use [clojure.tools.cli :only [parse-opts]])
   (:gen-class))
+
+(defn- normalize-path [path]
+  (.getCanonicalPath (io/as-file path)))
+
+(def ^:private cli-opts
+  [["-o" "--output" "Output directory, defaults to the current directory"
+    :default "."
+    :parse-fn normalize-path]
+   ["-h" "--help" "Print this help summary"]])
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [{dirs :arguments
+         opts :options
+         help :summary
+         err :errors} (parse-opts args cli-opts)]
+    (cond
+      (seq err)
+      (do (doseq [e err] (println e))
+          (println help))
+
+      (:help opts)
+      (println help))))
